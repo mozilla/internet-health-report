981d16267f4ec74860a44d6fdbf9dfcf65bf76bc
Marcel Legane <marcel@niceandserious.com>
2016-09-13 16:45:33 +0100

    Add responsive donut legend

--- src/javascripts/modules/donut.js
+++ src/javascripts/modules/donut.js
@@ -18,15 +18,18 @@ class Donut {
     this.data = data;
     this.width = $(this.el).width();
     this.radius = Math.min(this.width) * 0.5;
-    this.color = d3.scaleOrdinal()
-      .range(constants.colorRange);
     this.arc = d3.arc()
       .innerRadius((this.radius/10) * 7)
       .outerRadius(this.radius);
     this.pie = d3.pie()
       .value(d => d[dataKey])
       .sort(null);
-    this.classes = ['donut__svg', 'donut__g', 'donut__arc', 'donut__text'];
+    this.legendRectSize = 18;
+    this.legendSpacing = 4;
+    this.legendHeight = this.legendRectSize + this.legendSpacing;
+    this.color = d3.scaleOrdinal()
+      .range(constants.colorRange);
+    this.classes = ['donut__svg', 'donut__g', 'donut__arc', 'donut__text', 'donut__legend'];
   }
 
   render() {
@@ -57,13 +60,48 @@ class Donut {
       .style('fill', '#fff')
       .text(d => `${d.data.count}%`);
 
+    this.renderLegend();
+
     $(window).on('resize', this.resize.bind(this));
+  }
+
+  renderLegend() {
+    const donut = d3.select(this.el);
+
+    this.height = this.width + (this.color.domain().length * this.legendHeight);
+
+    donut.select(`.${this.classes[0]}`)
+      .attr('height', this.height);
+
+    const legend = donut.select(`.${this.classes[0]}`).selectAll(`.${this.classes[4]}`)
+      .data(this.color.domain())
+      .enter()
+      .append('g')
+        .attr('class', this.classes[4])
+        .attr('transform', (d, i) => {
+          const horz = 0;
+          const vert = this.width + (i * this.legendHeight);
+          return 'translate(' + horz + ',' + vert + ')';
+        });
+
+    legend.append('rect')
+      .attr('width', this.legendRectSize)
+      .attr('height', this.legendRectSize)
+      .style('fill', this.color)
+      .style('stroke', this.color);
+
+    legend.append('text')
+      .attr('x', this.legendRectSize + this.legendSpacing)
+      .attr('y', this.legendRectSize - this.legendSpacing)
+      .style('font-size', '12px')
+      .text(function(d) { return d; });
   }
 
   resize() {
     const donut = d3.select(this.el);
 
     this.width = $(this.el).width();
+    this.height = this.width + (this.color.domain().length * this.legendHeight);
     this.radius = Math.min(this.width) / 2;
     this.arc = d3.arc()
       .innerRadius((this.radius/10) * 7)
@@ -71,7 +109,7 @@ class Donut {
 
     donut.select(`.${this.classes[0]}`)
       .attr('width', this.width)
-      .attr('height', this.width);
+      .attr('height', this.height);
 
     donut.select(`.${this.classes[1]}`)
       .attr('transform', `translate(${this.radius}, ${this.radius})`);
@@ -81,6 +119,13 @@ class Donut {
 
     donut.selectAll(`.${this.classes[3]}`)
       .attr('transform', d => `translate(${this.arc.centroid(d)})`);
+
+    donut.selectAll(`.${this.classes[4]}`)
+      .attr('transform', (d, i) => {
+        const horz = 0;
+        const vert = this.width + (i * this.legendHeight);
+        return 'translate(' + horz + ',' + vert + ')';
+      });
   }
 }
 
