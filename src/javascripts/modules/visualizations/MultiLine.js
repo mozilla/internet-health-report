eb1e06c134ab18cc9b1108589a2212bf685a50aa
Marcel Legane <marcel@niceandserious.com>
2016-09-19 11:56:23 +0100

    Simplify multiline.js resize method

--- src/javascripts/modules/visualizations/MultiLine.js
+++ src/javascripts/modules/visualizations/MultiLine.js
@@ -9,7 +9,7 @@ class MultiLine {
     this.dataUrl = dataUrl;
     this.parseDate = d3.timeParse('%Y%m%d');
     this.margin = {top: 20, right: 20, bottom: 30, left: 40};
-    this.classes = ['multiline__svg', 'multiline__layer', 'multiline__dataset'];
+    this.classes = ['multiline__svg', 'multiline__data', 'multiline__dataset', 'x-axis', 'y-axis'];
     this.svg = d3.select(this.el)
       .append('svg')
         .attr('class', this.classes[0]);
@@ -22,23 +22,42 @@ class MultiLine {
     this.height = constants.getWindowWidth() < constants.breakpointM ? 400 : Math.ceil(this.width * 0.52);
     this.innerWidth = this.width - this.margin.left - this.margin.right;
     this.innerHeight = this.height - this.margin.top - this.margin.bottom;
+
+    this.svg
+      .attr('width', this.width)
+      .attr('height', this.height);
+
     this.x = d3.scaleTime()
       .range([0, this.innerWidth]);
     this.y = d3.scaleLinear()
       .range([this.innerHeight, 0]);
     this.z = d3.scaleOrdinal(d3.schemeCategory10);
+
     this.line = d3.line()
       .curve(d3.curveBasis)
       .x((d) => this.x(d.date))
-      .y((d) => this.y(d.temperature));
-    this.svg
-      .attr('width', this.width)
-      .attr('height', this.height);
+      .y((d) => this.y(d.dataValue));
+
+    this.x.domain(d3.extent(this.data, (d) => d.date));
+    this.y.domain([
+      d3.min(this.dataColumns, (c) => d3.min(c.values, (d) => d.dataValue)),
+      d3.max(this.dataColumns, (c) => d3.max(c.values, (d) => d.dataValue))
+    ]);
+    this.z.domain(this.dataColumns.map((c) => c.id));
+
+    this.svg.select(`.${this.classes[3]}`)
+      .attr('transform', `translate(0,${this.innerHeight})`)
+      .call(d3.axisBottom(this.x));
+
+    this.svg.select(`.${this.classes[4]}`)
+      .call(d3.axisLeft(this.y));
+
+    this.svg.selectAll(`.${this.classes[1]}`)
+      .attr('d', (d) => this.line(d.values))
+      .style('stroke', (d) => this.z(d.id));
   }
 
   render() {
-    this.setSizes();
-
     d3.tsv(this.dataUrl, this.type.bind(this), (error, data) => {
       if (error) throw error;
 
@@ -49,37 +68,26 @@ class MultiLine {
           values: data.map((d) => {
             return {
               date: d.date,
-              temperature: d[id]
+              dataValue: d[id]
             }
           })
         }
       });
 
-      this.x.domain(d3.extent(this.data, (d) => d.date));
-      this.y.domain([
-        d3.min(this.dataColumns, (c) => d3.min(c.values, (d) => d.temperature)),
-        d3.max(this.dataColumns, (c) => d3.max(c.values, (d) => d.temperature))
-      ]);
-      this.z.domain(this.dataColumns.map((c) => c.id));
+      this.g.append('g')
+        .attr('class', this.classes[3]);
 
       this.g.append('g')
-        .attr('class', 'axis axis--x')
-        .attr('transform', `translate(0,${this.innerHeight})`)
-        .call(d3.axisBottom(this.x));
+        .attr('class', this.classes[4]);
 
-      this.g.append('g')
-        .attr('class', 'axis axis--y')
-        .call(d3.axisLeft(this.y));
-
-      const city = this.g.selectAll(`.${this.classes[2]}`)
+      this.g.selectAll(`.${this.classes[2]}`)
         .data(this.dataColumns)
         .enter().append('g')
-          .attr('class', this.classes[2]);
+          .attr('class', this.classes[2])
+        .append('path')
+          .attr('class', this.classes[1]);
 
-      city.append('path')
-        .attr('class', this.classes[1])
-        .attr('d', (d) => this.line(d.values))
-        .style('stroke', (d) => this.z(d.id));
+      this.setSizes();
     });
 
     $(window).on('resize', this.resize.bind(this));
@@ -87,23 +95,6 @@ class MultiLine {
 
   resize() {
     this.setSizes();
-
-    this.x.domain(d3.extent(this.data, (d) => d.date));
-    this.y.domain([
-      d3.min(this.dataColumns, (c) => d3.min(c.values, (d) => d.temperature)),
-      d3.max(this.dataColumns, (c) => d3.max(c.values, (d) => d.temperature))
-    ]);
-    this.z.domain(this.dataColumns.map((c) => c.id));
-
-    this.svg.select('.axis--x')
-      .attr('transform', `translate(0,${this.innerHeight})`)
-      .call(d3.axisBottom(this.x));
-
-    this.svg.select('.axis--y')
-      .call(d3.axisLeft(this.y));
-
-    this.svg.selectAll(`.${this.classes[1]}`)
-      .attr('d', (d) => this.line(d.values));
   }
 
   type(d, _, columns) {
