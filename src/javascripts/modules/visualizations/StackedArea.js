71d603ffb4a0fb995a663dde01d1fff24082fcd2
Marcel Legane <marcel@niceandserious.com>
2016-09-19 15:57:49 +0100

    Simplify stackedarea.js resize method

--- src/javascripts/modules/visualizations/StackedArea.js
+++ src/javascripts/modules/visualizations/StackedArea.js
@@ -10,47 +10,63 @@ class StackedArea {
     this.el = el;
     this.dataUrl = dataUrl;
     this.margin = {top: 20, right: 20, bottom: 30, left: 40};
-    this.classes = ['stacked-area__svg', 'stacked-area__layer'];
+    this.classes = ['stacked-area__svg', 'stacked-area__layer', 'x-axis', 'y-axis'];
     this.svg = d3.select(this.el)
       .append('svg')
         .attr('class', this.classes[0]);
+    this.g = this.svg.append('g')
+      .attr('transform', `translate(${this.margin.left},${this.margin.top})`);
     this.z = d3.scaleOrdinal()
       .range(constants.colorRange);
     this.parseDate = d3.timeParse('%Y %b %d');
     this.stack = d3.stack();
-    this.g = this.svg.append('g')
-      .attr('transform', `translate(${this.margin.left},${this.margin.top})`);
   }
 
   setSizes() {
     this.width = $(this.el).width();
     this.height = constants.getWindowWidth() < constants.breakpointM ? 400 : Math.ceil(this.width * 0.52);
+    this.innerWidth = this.width - this.margin.left - this.margin.right;
+    this.innerHeight = this.height - this.margin.top - this.margin.bottom;
     this.svg
       .attr('width', this.width)
       .attr('height', this.height);
-    this.innerWidth = this.width - this.margin.left - this.margin.right;
-    this.innerHeight = this.height - this.margin.top - this.margin.bottom;
+
     this.x = d3.scaleTime()
       .range([0, this.innerWidth]);
+
     this.y = d3.scaleLinear()
       .range([this.innerHeight, 0]);
+
+    this.x.domain(d3.extent(this.data, (d) => d.date));
+    this.z.domain(this.keys);
+
     this.area = d3.area()
       .x((d, i) => this.x(d.data.date))
       .y0((d) => this.y(d[0]))
       .y1((d) => this.y(d[1]));
+
+    this.layer.selectAll('path')
+      .attr('d', this.area);
+
+    this.layer.selectAll('text')
+      .attr('x', this.innerWidth - 6)
+      .attr('y', (d) => this.y((d[d.length - 1][0] + d[d.length - 1][1]) / 2));
+
+    this.svg.select(`.${this.classes[2]}`)
+      .attr('transform', `translate(0,${this.innerHeight})`)
+      .call(d3.axisBottom(this.x));
+
+    this.svg.select(`.${this.classes[3]}`)
+      .call(d3.axisLeft(this.y).ticks(10, '%'));
   }
 
   render() {
-    this.setSizes();
-
     d3.tsv(this.dataUrl, this.type.bind(this), (error, data) => {
       if (error) throw error;
 
       this.data = data;
       this.keys = this.data.columns.slice(1);
 
-      this.x.domain(d3.extent(this.data, (d) => d.date));
-      this.z.domain(this.keys);
       this.stack.keys(this.keys);
 
       this.layer = this.g.selectAll(`.${this.classes[1]}`)
@@ -59,13 +75,10 @@ class StackedArea {
           .attr('class', this.classes[1]);
 
       this.layer.append('path')
-          .style('fill', (d) => this.z(d.key))
-          .attr('d', this.area);
+        .style('fill', (d) => this.z(d.key));
 
       this.layer.filter((d) => d[d.length - 1][1] - d[d.length - 1][0] > 0.01)
         .append('text')
-          .attr('x', this.innerWidth - 6)
-          .attr('y', (d) => this.y((d[d.length - 1][0] + d[d.length - 1][1]) / 2))
           .attr('dy', '.35em')
           .style('font', '10px sans-serif')
           .style('text-anchor', 'end')
@@ -74,14 +87,13 @@ class StackedArea {
 
       // render x-axis
       this.g.append('g')
-          .attr('class', 'axis axis--x')
-          .attr('transform', `translate(0,${this.innerHeight})`)
-          .call(d3.axisBottom(this.x));
+        .attr('class', this.classes[2]);
 
       // render y-axis
       this.g.append('g')
-          .attr('class', 'axis axis--y')
-          .call(d3.axisLeft(this.y).ticks(10, '%'));
+        .attr('class', this.classes[3]);
+
+      this.setSizes();
     });
 
     $(window).on('resize', this.resize.bind(this));
@@ -89,21 +101,6 @@ class StackedArea {
 
   resize() {
     this.setSizes();
-
-    this.x.domain(d3.extent(this.data, (d) => d.date));
-    this.layer.selectAll('path')
-      .attr('d', this.area);
-
-    this.layer.selectAll('text')
-      .attr('x', this.innerWidth - 6)
-      .attr('y', (d) => this.y((d[d.length - 1][0] + d[d.length - 1][1]) / 2));
-
-    this.g.select('.axis--x')
-      .attr('transform', `translate(0,${this.innerHeight})`)
-      .call(d3.axisBottom(this.x));
-
-    this.g.select('.axis--y')
-      .call(d3.axisLeft(this.y).ticks(10, '%'));
   }
 
   type(d, i, columns) {
