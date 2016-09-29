012c855c11cd93457ed1fc07accc4530fe3d8a3c
Marcel Legane <marcel@niceandserious.com>
2016-09-29 16:31:11 +0100

    Add fixed header navigation on scroll

---
+++ src/javascripts/modules/interaction/fixNav.js
@@ -0,0 +1,17 @@
+import * as constants from '../constants';
+import $ from 'jquery';
+window.$ = $;
+
+const fixedHeaderNavClass = `is-fixed`;
+
+const fixNav = (scrollTop) => {
+  const $headerNav = $(`.js-header-nav`);
+
+  if (scrollTop >= constants.headerNavOffsetTop && !$headerNav.hasClass(fixedHeaderNavClass)) {
+    $headerNav.addClass(fixedHeaderNavClass);
+  } else if (scrollTop < constants.headerNavOffsetTop && $headerNav.hasClass(fixedHeaderNavClass)) {
+    $headerNav.removeClass(fixedHeaderNavClass);
+  }
+};
+
+export { fixNav };
