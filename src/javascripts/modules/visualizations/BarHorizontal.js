6c4dbe3a3c27dd8c56c8ac70cb34644b3cba53f7
Marcel Legane <marcel@niceandserious.com>
2016-10-06 11:20:41 +0100

    Center label text on horizontal bar chart

--- src/javascripts/modules/visualizations/BarHorizontal.js
+++ src/javascripts/modules/visualizations/BarHorizontal.js
@@ -49,7 +49,9 @@ class BarHorizontal {
       .call(this.axisBottom);
 
     this.svg.select(`.${this.classes[3]}`)
-      .call(d3.axisLeft(this.y));
+      .call(d3.axisLeft(this.y))
+      .selectAll(`text`)
+        .call(this.wrap, this.margin.left);
   }
 
   render() {
@@ -88,6 +90,49 @@ class BarHorizontal {
   type(d) {
     d[this.dataKeys[1]] = +d[this.dataKeys[1]];
     return d;
+  }
+
+  wrap(text, width) {
+    text.each(function() {
+      const textEl = d3.select(this);
+      const words = textEl.text().split(/\s+/).reverse();
+      const lineHeight = 1.4;
+      const y = textEl.attr(`y`);
+      const dy = parseFloat(textEl.attr(`dy`));
+      const yAxisOffset = 10;
+      let word;
+      let textHeight;
+      let line = [];
+      let lineLength = 0;
+      let lineNumber = 0;
+      let tspan = textEl.text(null)
+        .append(`tspan`)
+        .attr(`x`, 0)
+        .attr(`y`, y)
+        .attr(`dy`, dy + `em`);
+
+      while (words.length > 0) {
+        word = words.pop();
+        line.push(word);
+        tspan.text(line.join(` `));
+
+        if (tspan.node().getComputedTextLength() > width - yAxisOffset) {
+          lineLength++;
+          line.pop();
+          tspan.text(line.join(` `));
+          line = [word];
+          tspan = textEl
+            .append(`tspan`)
+            .attr(`x`, 0)
+            .attr(`y`, y)
+            .attr(`dy`, ++lineNumber * lineHeight + dy + `em`)
+            .text(word);
+        }
+      }
+
+      textHeight = textEl.node().getBBox().height;
+      textEl.attr(`transform`, `translate(${-yAxisOffset},${(textHeight / (lineLength + 1)) * (lineLength * -0.5)})`);
+    });
   }
 }
 
