ae75b12318cd27a0b4f1e05f2e1d306e5ca715e5
Marcel Legane <marcel@niceandserious.com>
2016-10-10 10:21:55 +0100

    Add https-share line chart

--- src/javascripts/modules/visualizations/Line.js
+++ src/javascripts/modules/visualizations/Line.js
@@ -9,17 +9,16 @@ class Line {
     this.dataUrl = dataUrl;
     this.parseDate = d3.timeParse(`%d-%b-%y`);
     this.margin = {top: 20, right: 20, bottom: 30, left: 40};
-    this.classes = [`line__svg`, `line__data`, `x-axis`, `y-axis`];
-    this.svg = d3.select(this.el)
-      .append(`svg`)
-        .attr(`class`, this.classes[0]);
+    this.classes = [`line__svg`, `line__data`, `x-axis`, `y-axis`, `line__dot`];
+    this.svg = d3.select(this.el).append(`svg`)
+      .attr(`class`, this.classes[0]);
     this.svgData = this.svg.append(`g`)
       .attr(`transform`, `translate(${this.margin.left},${this.margin.top})`);
   }
 
   setSizes() {
     this.width = $(this.el).width();
-    this.height = constants.getWindowWidth() < constants.breakpointM ? 400 : Math.ceil(this.width * 0.52);
+    this.height = constants.getWindowWidth() < constants.breakpointM ? 300 : Math.ceil(this.width * 0.52);
     this.innerWidth = this.width - this.margin.left - this.margin.right;
     this.innerHeight = this.height - this.margin.top - this.margin.bottom;
 
@@ -30,39 +29,59 @@ class Line {
       .range([this.innerHeight, 0]);
 
     this.line = d3.line()
-      .curve(d3.curveBasis)
-      .x((d) => this.x(d.date))
-      .y((d) => this.y(d.close));
+      // .curve(d3.curveBasis)
+      .x((d) => this.x(d[this.dataKeys[0]]))
+      .y((d) => this.y(d[this.dataKeys[1]]));
 
     this.svg
       .attr(`width`, this.width)
       .attr(`height`, this.height);
 
-    this.x.domain(d3.extent(this.data, (d) => d.date));
-    this.y.domain([0, d3.max(this.data, (d) => d.close)]);
+    this.x.domain([this.minDate - this.xPad, this.maxDate + this.xPad]);
+    this.y.domain([d3.min(this.data, (d) => d[this.dataKeys[1]]) - 1, d3.max(this.data, (d) => d[this.dataKeys[1]])]);
 
     this.svg.select(`.${this.classes[1]}`)
       .attr(`d`, this.line);
 
+    this.svg.selectAll(`.${this.classes[4]}`)
+      .attr(`cx`, d => this.x(d[this.dataKeys[0]]))
+      .attr(`cy`, d => this.y(d[this.dataKeys[1]]));
+
+    this.axisBottom = constants.getWindowWidth() < constants.breakpointM ? d3.axisBottom(this.x).ticks(4).tickFormat(d3.timeFormat(`%b`)) : d3.axisBottom(this.x).tickFormat(d3.timeFormat(`%b`));
+
     this.svg.select(`.${this.classes[2]}`)
       .attr(`transform`, `translate(0,${this.innerHeight})`)
-      .call(d3.axisBottom(this.x));
+      .call(this.axisBottom);
 
     this.svg.select(`.${this.classes[3]}`)
       .call(d3.axisLeft(this.y));
   }
 
   render() {
-    d3.tsv(this.dataUrl, this.type.bind(this), (error, data) => {
+    d3.tsv(this.dataUrl, (error, data) => {
       if (error) {
         throw error;
       }
 
       this.data = data;
+      this.dataKeys = constants.getDataKeys(this.data);
+
+      this.data.forEach(this.type.bind(this));
+
+      this.minDate = d3.min(this.data, d => d[this.dataKeys[0]].getTime());
+      this.maxDate = d3.max(this.data, d => d[this.dataKeys[0]].getTime());
+      this.xPad = (this.maxDate - this.minDate) * 0.05;
 
       this.svgData.append(`path`)
         .datum(this.data)
         .attr(`class`, this.classes[1]);
+
+      // append scatterplot dots
+      this.svgData.selectAll(this.classes[4])
+        .data(this.data)
+        .enter().append(`circle`)
+          .attr(`class`, this.classes[4])
+          .attr(`r`, 4.5);
 
       // append x-axis
       this.svgData.append(`g`)
@@ -83,8 +102,8 @@ class Line {
   }
 
   type(d) {
-    d.date = this.parseDate(d.date);
-    d.close = +d.close;
+    d[this.dataKeys[0]] = this.parseDate(d[this.dataKeys[0]]);
+    d[this.dataKeys[1]] = +d[this.dataKeys[1]];
     return d;
   }
 }
