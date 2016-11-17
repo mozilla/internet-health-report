cfe69d10875c8f1a492787eec076b9fe564b8b28
Marcel Legane <marcel@niceandserious.com>
2016-11-17 10:45:54 +0000

    Correct point at which nav is fixed on scroll

--- wp-content/themes/theinternethealthreport/src/javascripts/modules/interaction/fixNav.js
+++ wp-content/themes/theinternethealthreport/src/javascripts/modules/interaction/fixNav.js
@@ -7,9 +7,9 @@ const fixedHeaderNavClass = `is-fixed`;
 const fixNav = (scrollTop) => {
   const $headerNav = $(`.js-header-nav`);
 
-  if (scrollTop >= constants.headerNavOffsetTop && !$headerNav.hasClass(fixedHeaderNavClass)) {
+  if (scrollTop > constants.headerNavOffsetTop && !$headerNav.hasClass(fixedHeaderNavClass)) {
     $headerNav.addClass(fixedHeaderNavClass);
-  } else if (scrollTop < constants.headerNavOffsetTop && $headerNav.hasClass(fixedHeaderNavClass)) {
+  } else if (scrollTop <= constants.headerNavOffsetTop && $headerNav.hasClass(fixedHeaderNavClass)) {
     $headerNav.removeClass(fixedHeaderNavClass);
   }
 };
