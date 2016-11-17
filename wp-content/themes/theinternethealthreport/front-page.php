d66b278efd87f8d9485f7e6b782b52aefb277ea9
Marcel Legane <marcel@niceandserious.com>
2016-11-17 11:53:26 +0000

    Add home intro section

--- wp-content/themes/theinternethealthreport/front-page.php
+++ wp-content/themes/theinternethealthreport/front-page.php
@@ -1,5 +1,12 @@
 <?php get_header(); ?>
 
+<div class="wrapper">
+  <div class="intro">
+    <p class="intro__text"><?php the_field('intro_text'); ?></p>
+    <a class="intro__link" href="<?php the_field('intro_page_link'); ?>"><?php the_field('intro_page_link_text'); ?> <?php echo get_template_part('assets/icons/icon', 'link.svg'); ?></a>
+  </div>
+</div>
+
 <?php get_template_part('content', 'section-nav'); ?>
 
 <?php get_footer(); ?>
