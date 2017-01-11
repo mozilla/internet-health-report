3b40dcee3a5670fa27576180f82e1262673d6998
Marcel Legane <marcel@niceandserious.com>
2017-01-11 16:37:02 +0000

    Add facebook og tags

--- wp-content/themes/theinternethealthreport/functions.php
+++ wp-content/themes/theinternethealthreport/functions.php
@@ -225,4 +225,33 @@ function language_selector() {
   }
 }
 
+//Adding the Open Graph in the Language Attributes
+function add_opengraph_doctype( $output ) {
+    return $output . ' xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml"';
+  }
+add_filter('language_attributes', 'add_opengraph_doctype');
+
+//Lets add Open Graph Meta Info
+
+function insert_fb_in_head() {
+  global $post;
+  if ( !is_singular()) //if it is not a post or a page
+    return;
+        echo '<meta property="og:title" content="' . get_the_title() . '"/>';
+        echo '<meta property="og:type" content="article"/>';
+        echo '<meta property="og:url" content="' . get_permalink() . '"/>';
+        echo '<meta property="og:site_name" content="' . get_bloginfo('name') . '"/>';
+  if(!has_post_thumbnail( $post->ID )) { //the post does not have featured image, use a default image
+    $default_image = get_template_directory_uri() . '/assets/images/theinternethealthreport.svg'; //replace this with a default image on your server or an image in your media library
+    echo '<meta property="og:image" content="' . $default_image . '"/>';
+  }
+  else{
+    $thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
+    echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '"/>';
+  }
+  echo "
+";
+}
+add_action( 'wp_head', 'insert_fb_in_head', 5 );
+
 ?>
